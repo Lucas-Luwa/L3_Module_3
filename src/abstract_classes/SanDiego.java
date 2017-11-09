3deca97a69874406e7b086b1907531ecc7c078ea
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-11-08 18:56:23 -0800

    11/8.17
    Module 3 still ip

--- src/abstract_classes/SanDiego.java
+++ src/abstract_classes/SanDiego.java
@@ -1,5 +1,15 @@
 package abstract_classes;
 
-public class SanDiego {
+public class SanDiego extends City{
 
+	public SanDiego(int population, double growthRate) {
+		super(population, growthRate);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	double getAnnualTaxes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
